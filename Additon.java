7e7707168fb01283c5a3167b794db3b4c016ae7d
ashupant01 <ashupant94@gmail.com>
2022-03-24 17:19:22 +0530

    day 1 programs

---
+++ Additon.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Additon {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int sum=x+y ;
+		System.out.println("sum of two no is :" +sum);
+	    
+
+	}
+}
---
+++ SameString.java
@@ -0,0 +1,30 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class SameString {
+
+	public static void main(String[] args)
+	{
+		
+		{
+			{
+		        String str1, str2;
+		        Scanner scanner = new Scanner(System.in);
+		 
+		        System.out.println("Enter first String");
+		        str1 = scanner.nextLine();
+		 
+		        System.out.println("Enter second String");
+		        str2 = scanner.nextLine();
+		        
+		        if (str1.equals(str2))
+		            System.out.print("Equal Strings");
+		        else
+		            System.out.print("UnEqual Strings");
+		    }
+		}
+
+	}
+
+}
